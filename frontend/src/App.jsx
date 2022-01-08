7dfce2866ef38754e240608d512a07c7e613d561
Aryan Patel <69769265+AryPat@users.noreply.github.com>
2022-01-08 14:35:12 -0500

    changing name of csv file when downloading

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -99,7 +99,7 @@ function App() {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "file.csv"); //or any other extension
+      link.setAttribute("download", `Inventory-${new Date().toISOString()}.csv`); //or any other extension
       document.body.appendChild(link);
       link.click();
     });
